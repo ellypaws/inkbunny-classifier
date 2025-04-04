6393a77893c82f9e950d725cb209557c01c23e49
Elly <145195760+ellypaws@users.noreply.github.com>
2025-04-05 07:45:02 +0800

    feat: add tests and benchmarks for PredictURL and DefaultCache

--- pkg/classify/classify_test.go
+++ pkg/classify/classify_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	_ "embed"
+	"net/http"
+	"sync"
 	"testing"
 	"time"
 )
@@ -29,5 +31,68 @@ func TestCache_Predict(t *testing.T) {
 			t.Fatal(err)
 		}
 		t.Logf("Prediction: %v, Time: %v", prediction, time.Since(now))
+	}
+}
+
+const imagePath = "http://localhost:8000/image.png"
+
+var proxy sync.Once
+
+func start() {
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write(file)
+		if err != nil {
+			panic(err)
+		}
+	})
+	go http.ListenAndServe("localhost:8000", nil)
+}
+
+var warmupOnce sync.Once
+
+func warmup() {
+	proxy.Do(start)
+	warmupOnce.Do(func() { PredictURL(context.Background(), imagePath) })
+	DefaultCache.reset()
+}
+
+func TestPredictURL(t *testing.T) {
+	proxy.Do(start)
+	warmup()
+	prediction, err := PredictURL(context.Background(), imagePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Prediction: %v", prediction)
+}
+
+func TestCache_PredictURL(t *testing.T) {
+	proxy.Do(start)
+	warmup()
+	DefaultCache.reset()
+	prediction, err := DefaultCache.PredictURL(context.Background(), imagePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Prediction: %v", prediction)
+}
+
+func BenchmarkPredict(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := Predict(context.Background(), image)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkPredictURL(b *testing.B) {
+	proxy.Do(start)
+	warmup()
+	for i := 0; i < b.N; i++ {
+		_, err := PredictURL(context.Background(), imagePath)
+		if err != nil {
+			b.Fatal(err)
+		}
 	}
 }
