5fb45033b04b7a6d5336ea13fb20157b0f9e22f8
Elly <145195760+ellypaws@users.noreply.github.com>
2025-04-10 10:32:26 +0800

    fix: update file path for serving the main HTML page

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -25,7 +25,7 @@ var index []byte
 // HomeHandler serves the main HTML page
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	http.ServeFile(w, r, "index.html")
+	http.ServeFile(w, r, "pkg/server/index.html")
 }
 
 func FileProxy(w http.ResponseWriter, r *http.Request) {
