cdcd97aabfe12ddb57cb8a5f8fe27e758dd556ae
Elly <145195760+ellypaws@users.noreply.github.com>
2025-04-17 01:40:42 +0800

    feat: use signal.NotifyContext for graceful shutdown in telegram/main.go

--- cmd/telegram/main.go
+++ cmd/telegram/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"os/signal"
 
 	"github.com/charmbracelet/log"
 
@@ -20,7 +21,8 @@ const (
 
 func main() {
 	defer utils.LogOutput(os.Stdout)()
-	ctx, done := context.WithCancel(context.Background())
+	ctx, done := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer done()
 	b, err := bot.New(bot.Config{
 		Output:        os.Stdout,
 		Token:         os.Getenv(EnvTelegramBotToken),
@@ -34,15 +36,10 @@ func main() {
 		log.Fatalf("error creating bot: %v", err)
 	}
 
-	go func() {
-		err = b.Start()
-		if err != nil {
-			log.Fatalf("error starting bot: %v", err)
-		}
-	}()
+	if err := b.Start(); err != nil {
+		log.Fatalf("error starting bot: %v", err)
+	}
 
-	b.Wait()
-	done()
 	if err := b.Shutdown(); err != nil {
 		log.Fatalf("error shutting down bot: %v", err)
 	}
