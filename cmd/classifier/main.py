0b42949e72659fac24e581acdb0ab534d840387f
Elly <145195760+ellypaws@users.noreply.github.com>
2025-04-18 00:57:28 +0800

    feat: set classifier port

--- cmd/classifier/main.py
+++ cmd/classifier/main.py
@@ -113,4 +113,5 @@ app = gr.mount_gradio_app(app, gr_interface, path="")
 
 # Run server
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    port = int(os.getenv('PORT', '7860'))
+    uvicorn.run(app, host="0.0.0.0", port=port)
